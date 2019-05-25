0b88a3553a4270252e50449581f3c33018fe908f
はえ <noboru.tyrannosaurus@gmail.com>
2019-05-25 17:11:51 +0900

    とりあえず

--- src/main/java/jp/kps8x9/middle2/kps8x9m2gameplugin/KPS8x9M2gamePlugin.java
+++ src/main/java/jp/kps8x9/middle2/kps8x9m2gameplugin/KPS8x9M2gamePlugin.java
@@ -1,21 +1,11 @@
 package jp.kps8x9.middle2.kps8x9m2gameplugin;
 
-<<<<<<< HEAD
-import jp.kps8x9.middle2.kps8x9m2gameplugin.util.ClassUtil;
 import jp.kps8x9.middle2.kps8x9m2gameplugin.util.GameStart;
-import org.bukkit.plugin.java.JavaPlugin;
-
 import java.util.Objects;
-
 import static com.sun.javafx.binding.Logging.getLogger;
-=======
 import jp.kps8x9.middle2.kps8x9m2gameplugin.Event.NexusDamage;
 import jp.kps8x9.commons.util.ClassUtil;
 import jp.kps8x9.middle2.kps8x9m2gameplugin.util.Scouter;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.plugin.java.JavaPlugin;
-
->>>>>>> d5a5260e9955cc038eae1db15310efe06a2474d9
 
 public final class KPS8x9M2gamePlugin extends JavaPlugin {
 
@@ -35,15 +25,13 @@ public final class KPS8x9M2gamePlugin extends JavaPlugin {
         // コマンドをここに登録
         // plugin.yml への記載も忘れずに！
         // ----------------------------------------
-<<<<<<< HEAD
+
         Objects.requireNonNull(this.getCommand("m2test")).setExecutor(new M2TestCommand(this));
         Objects.requireNonNull(this.getCommand("start")).setExecutor(new GameStart());
-=======
         MHCommand cmd = new MHCommand(this);
         this.getCommand("m2test").setExecutor(new M2TestCommand(this));
         this.getCommand("mh").setExecutor(cmd);
         this.getCommand("finish").setExecutor(new FinishEvent(this));
->>>>>>> d5a5260e9955cc038eae1db15310efe06a2474d9
 
         // ----------------------------------------
         // イベントリスナーをここに登録
